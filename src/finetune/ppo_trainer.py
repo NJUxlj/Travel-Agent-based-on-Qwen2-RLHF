6763bfb9a93b799b8a81afe9b1f358626ec8a2e9
NJUxlj <1016509070@qq.com>
2025-04-08 00:03:59 +0800

    update ppo trainer

--- src/finetune/ppo_trainer.py
+++ src/finetune/ppo_trainer.py
@@ -1,4 +1,5 @@
 import torch  
+import torch.nn as nn
 from torch.utils.data import Dataset  
 import torch.nn.functional as F  
 from transformers import (  
@@ -6,7 +7,10 @@ from transformers import (
     TrainingArguments,  
     Trainer,  
     TrainerCallback,  
-    AutoModelForSequenceClassification
+    AutoModelForSequenceClassification,
+    DebertaV2Model,
+    
+    DebertaV2Config
 )  
 
 
@@ -15,7 +19,14 @@ from tqdm import tqdm
 
 from src.models.qwen2.modeling_qwen2 import Qwen2ForCausalLM
 
-from src.configs.config import REWARD_MODEL_PATH, MODEL_PATH, SFT_MODEL_PATH, PPO_MODEL_PATH, DPO_DATA_PATH
+from src.configs.config import (
+    REWARD_MODEL_PATH, 
+    MODEL_PATH, 
+    SFT_MODEL_PATH, 
+    PPO_MODEL_PATH, 
+    DPO_DATA_PATH,
+    CACHED_DPO_DATA_PATH
+)
 
 
 from peft import LoraConfig, get_peft_model  
@@ -39,6 +50,60 @@ class PPODataset(Dataset):
             "response_ids": self.data["response_ids"][idx],  
             "old_log_probs": self.data["old_log_probs"][idx],  
         }  
+        
+        
+        
+        
+class Critic(nn.Module):
+    '''
+    # 价值（评论家）模型：
+    #   - 用于预测每一步（生成token）的动作产生的收益，使用演员模型进行初始化，并外加一个回归头，输出shape为：(batch_size, seq_len， 1)
+    #   - 通过策略模型初始化得到
+    
+    '''
+    def __init__(self, base_model):
+        super().__init__()
+        self.base_model = base_model  # 可以看做一个 encoder
+        self.base_model.eval()  # 冻结骨干部分
+        self.value_head = nn.Linear(base_model.config.hidden_size, 1)
+        
+    def forward(
+            self, 
+            input_ids = None, 
+            inputs_embeds = None,
+            attention_mask = None, 
+            num_actions=None
+            ):
+        '''
+        num_actions: 模型新推理了多少token
+        
+        return values: shape = (batch_size, seq_len)
+        '''
+        
+        assert input_ids is not None or inputs_embeds is not None, f"one of the input_ids and inputs_embeds has to be not None"
+        
+        
+        if input_ids is not None:
+            hidden_states = self.base_model.forward(
+                input_ids = input_ids,
+                attention_mask = attention_mask
+            ).last_hidden_state  # shape = (batch_size, seq_len, hidden_size)
+        else:
+            hidden_states = self.base_model.forward(
+                inputs_embeds = inputs_embeds,
+                attention_mask = attention_mask
+            ).last_hidden_state
+        
+        value_model_output = self.value_head.forward(hidden_states)  # shape = (batch_size, seq_len, 1)
+        
+        # values = value_model_output.squeeze(-1)[:, -num_actions:] # 只取策略做出的actions（response）的部分
+        
+        values = value_model_output.squeeze(-1) # 只取策略做出的actions（response）的部分
+        
+        
+        return values
+    
+        
 
 class PPOTrainer:  
     """  
@@ -49,6 +114,7 @@ class PPOTrainer:
         self,  
         output_dir: str = PPO_MODEL_PATH,  
         dataset_path: str = DPO_DATA_PATH,  
+        cached_data_path:str = CACHED_DPO_DATA_PATH,
         model_name: str = MODEL_PATH,  # 基础模型名称
         reward_model_name:str = REWARD_MODEL_PATH,
         clip_epsilon: float = 0.2,     # PPO裁剪参数，限制策略更新幅度
@@ -61,6 +127,11 @@ class PPOTrainer:
         peft_config: LoraConfig = None  
     ):  
         self.output_dir = output_dir  
+        
+        self.dataset_path = dataset_path
+        self.cached_data_path = cached_data_path
+        self.max_seq_length = max_seq_length
+        
         self.clip_epsilon = clip_epsilon  
         self.gamma = gamma  
         self.gae_lambda = gae_lambda  
@@ -70,6 +141,8 @@ class PPOTrainer:
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)  
         self.tokenizer.pad_token = self.tokenizer.eos_token  
         
+        self.reward_tokenizer = AutoTokenizer.from_pretrained(reward_model_name)
+        
         # 创建主策略模型（Actor）  - 这是我们要优化的模型 
         self.model = Qwen2ForCausalLM.from_pretrained(  
             model_name,  
@@ -77,34 +150,34 @@ class PPOTrainer:
             trust_remote_code=True  
         )  
         
+        self.device  =self.model.device
+        
         # 创建参考策略模型 - 用于KL散度计算，防止策略偏离太远  
         # 类似于RLHF中的SFT模型，作为行为基准  
         self.ref_model = Qwen2ForCausalLM.from_pretrained(  
             model_name,  
-            device_map="auto",  
+            # device_map="auto",  
             trust_remote_code=True  
-        )  
-        # 冻结参考模型  
-        for param in self.ref_model.parameters():  
-            param.requires_grad = False  
+        ).to(self.device)
+
             
             
             
         # 创建奖励模型    
         self.reward_model = AutoModelForSequenceClassification.from_pretrained(
             reward_model_name,
-            device_map="auto",
             trust_remote_code=True
-        )
-        for param in self.reward_model.parameters():
-            param.requires_grad = False
+        ).to(self.device)
+        
         
         # 创建价值头网络（Critic）- 用于预测状态值函数V(s)  
         # 在PPO中，Actor-Critic架构是标准做法   
-        self.value_head = torch.nn.Linear(  
-            self.model.config.hidden_size,    # 输入维度：模型隐藏状态大小 
-            1                                 # 输出维度：单一值，表示状态价值  
-        ).to(self.model.device)  
+        # self.value_head = torch.nn.Linear(  
+        #     self.model.config.hidden_size,    # 输入维度：模型隐藏状态大小 
+        #     1                                 # 输出维度：单一值，表示状态价值  
+        # ).to(self.model.device)  
+        
+        self.critic_model = Critic(self.model.base_model).to(self.device)
         
         # 应用LoRA  
         if is_peft:  
@@ -112,28 +185,49 @@ class PPOTrainer:
             self.model = get_peft_model(self.model, self.peft_config)  
 
         # 准备训练数据集  
-        self.dataset = self._prepare_dataset(dataset_path, max_seq_length)  
+        self.dataset = self._load_cached_dataset(self.cached_data_path)  
 
         # 配置优化器 - 同时优化策略模型和价值头   
-        self.optimizer = torch.optim.AdamW(  
-            list(self.model.parameters()) + list(self.value_head.parameters()),   
-            lr=lr,  
-            weight_decay=0.01   # 权重衰减，防止过拟合  
-        )  
+        
+        # [方案1， 可以用， 但是有点奇怪]
+        # self.optimizer = torch.optim.AdamW(  
+        #     list(self.model.parameters()) + list(self.value_head.parameters()),   
+        #     lr=lr,  
+        #     weight_decay=0.01   # 权重衰减，防止过拟合  
+        # )  
+        
+        # 【方案2， 分别为 actor和critic创建优化器】
+        self.optimizer_actor = torch.optim.Adam(self.model.parameters(), lr=0.00005)
+        self.optimizer_critic = torch.optim.Adam(self.critic_model.parameters(), lr=0.00005)
+        
 
     def _default_lora_config(self):  
         return LoraConfig(  
-            r=64,  
+            r=32,  
             lora_alpha=16,  
             lora_dropout=0.05,  
-            target_modules=["q_proj", "v_proj"],  
+            target_modules=["q_proj", "k_proj" ,"v_proj"],  
             bias="none",  
             task_type="CAUSAL_LM"  
         )  
+        
+        
+    def _load_cached_dataset(self, dataset_path=CACHED_DPO_DATA_PATH):
+        if not os.path.exists(dataset_path):
+            os.makedirs(CACHED_DPO_DATA_PATH, exist_ok=True)
+        try:
+            tokenized_data = load_from_disk(dataset_path)
+            print("从缓存加载DPO数据集成功~~~")
+            tokenized_data.set_format(type="torch")
+            return PPODataset(tokenized_data)
+        except Exception as e:
+            print(f"加载缓存的DPO数据集（tokenized）失败: {e}, 将重新预处理数据")
+            ppo_dataset = self._prepare_dataset(self.dataset_path, self.max_seq_length)
+            return ppo_dataset
 
-    def _prepare_dataset(self, dataset_path, max_length):  
+    def _prepare_dataset(self, dataset_path, max_length, split='train'):  
         """数据预处理流程"""  
-        dataset = load_dataset(dataset_path, split="train").select(range(1000))  
+        dataset = load_dataset(dataset_path, split=split).select(range(500))  
         
         def process_fn(samples):  
             batch = {"input_ids": [], "attention_mask": [],  
@@ -150,19 +244,30 @@ class PPOTrainer:
                     padding="max_length",  
                     truncation=True,  
                     return_tensors="pt"  
-                ).to(self.model.device)
+                ).to(self.device)
                 
-                # 计算旧策略概率  
+                # 计算旧策略概率  (使用多项式采样替代argmax)
                 with torch.no_grad():  
-                    logits = self.model(**tokens).logits.to(self.model.device)
+                    logits = self.model.forward(**tokens).logits.to(self.device)
                     log_probs = F.log_softmax(logits, dim=-1)  
+                    
+                    # 多项式采样
+                    probs = torch.softmax(logits, dim=-1)
+                    response_ids = [] # 贪心采样， 可以改成多项式采样或者top-k，
+                    for i in range(probs.size(1)):  # 遍历每个token位置
+                        token_probs = probs[:, i, :]  # 获取当前token位置的概率分布 (batch_size, vocab_size)
+                        sampled_ids = torch.multinomial(token_probs, num_samples=1)  # (batch_size, 1)
+                        response_ids.append(sampled_ids)
+                    response_ids = torch.cat(response_ids, dim=-1)  # (batch_size, seq_len)
+                    
+                    # 只收集生成部分的log_prob
                     old_log_probs = torch.gather(  
-                        log_probs, -1, tokens["input_ids"].unsqueeze(-1)    # 把整个序列的logprobs都收集起来了，而不是仅仅收集action的部分
+                        log_probs, -1, response_ids.unsqueeze(-1)    # 把整个序列的logprobs都收集起来了，而不是仅仅收集action的部分
                     ).squeeze(-1)    # shape = (batch_size, seq_len)
                 
                 batch["input_ids"].append(tokens["input_ids"][0])  
                 batch["attention_mask"].append(tokens["attention_mask"][0])  
-                batch["response_ids"].append(tokens["input_ids"][0])  
+                batch["response_ids"].append(response_ids[0])  
                 batch["old_log_probs"].append(old_log_probs[0])  
             
             return batch  
@@ -174,8 +279,14 @@ class PPOTrainer:
             remove_columns=dataset.column_names  
         )  
         
-        tokenized_data.set_format(type="torch")
         
+        
+        # 保存预处理好的数据集到本地
+        if not os.path.exists(CACHED_DPO_DATA_PATH):
+            os.makedirs(CACHED_DPO_DATA_PATH, exist_ok=True)
+        tokenized_data.save_to_disk(CACHED_DPO_DATA_PATH)
+        
+        tokenized_data.set_format(type="torch")
         
         return PPODataset(tokenized_data)  
 
@@ -197,16 +308,28 @@ class PPOTrainer:
         
         ###Return
         
-        """  
-        # 这里示例使用简单奖励函数（可以替换为实际奖励函数）  
-        # 例如，使用生成文本的长度作为正向奖励  
-        rewards = []  
         
+        ### 简介
+            这里示例使用简单奖励函数（可以替换为实际奖励函数）
+            例如，使用生成文本的长度作为正向奖励  
+        
+        """  
+        
+        # 转换成文本
+        response_text = self.tokenizer.batch_decode(response_ids, skip_special_tokens = True)
+        
+        reward_model_inputs  =  self.reward_tokenizer(response_text, return_tensors="pt", padding=True)
+        
+        reward_model_inputs = {k: v.to(self.reward_model.device) for k, v in reward_model_inputs.items()}
         # 使用reward model计算外部奖励
         with torch.no_grad():
+            
+            print("reward_model_inputs['input_ids'].shape = ", reward_model_inputs['input_ids'].shape)
+            print("deberta_v2.config.max_length = ", DebertaV2Config().max_position_embeddings)
+            
             reward_outputs = self.reward_model(
-                input_ids=response_ids,
-                attention_mask=attention_mask
+                input_ids=reward_model_inputs['input_ids'],
+                attention_mask=reward_model_inputs['attention_mask']
             )  # shape = (bsz, )
             external_rewards = reward_outputs.logits.squeeze(-1)
         
@@ -265,11 +388,12 @@ class PPOTrainer:
         
         return advantages, returns  
 
-    def compute_loss(self, model, inputs):  
+    def compute_loss(self, actor_model:Qwen2ForCausalLM, critic_model:Critic, inputs):  
         # 前向传播获取logits  
-        outputs = model(  
+        outputs = actor_model.forward(  
             input_ids=inputs["input_ids"],  
-            attention_mask=inputs["attention_mask"]  
+            attention_mask=inputs["attention_mask"] ,
+            output_hidden_states=True
         )  
         
         # 计算奖励和KL散度  
@@ -280,10 +404,10 @@ class PPOTrainer:
         )  
         
         # 获取隐藏状态用于价值估计  
-        hidden_states = outputs.hidden_states[-1]  # 获取最后一层的隐藏状态  
+        # hidden_states = outputs.hidden_states[-1]  # 获取最后一层的隐藏状态  
         
         # 使用价值头计算每个token的价值  
-        values = self.value_head(hidden_states).squeeze(-1)  
+        values = critic_model.forward(input_ids = inputs["input_ids"], attention_mask=inputs["attention_mask"])   
         
         # 计算蒙版（用于忽略padding）  
         masks = inputs["attention_mask"]  
@@ -324,7 +448,7 @@ class PPOTrainer:
         """自定义训练循环"""  
         train_args = TrainingArguments(  
             output_dir=self.output_dir,  
-            per_device_train_batch_size=4,  
+            per_device_train_batch_size=2,  
             gradient_accumulation_steps=2,  
             learning_rate=3e-5,  
             logging_steps=10,  
@@ -354,30 +478,42 @@ class PPOTrainer:
         
         # PPO多轮优化  
         for epoch in range(self.ppo_epochs):
-            progress_bar = tqdm(train_dataloader, desc=f"Epoch {epoch}")
+            progress_bar = tqdm(train_dataloader, desc=f"PPO Epoch {epoch}")
             
             for batch in progress_bar:
-                self.optimizer.zero_grad()
+                
+                batch = {k: v.to(self.model.device) for k, v in batch.items()}
+                self.optimizer_actor.zero_grad()
+                
+                self.optimizer_critic.zero_grad()
                 
                 # 前向传播
                 outputs = self.model(
                     input_ids=batch["input_ids"],
-                    attention_mask=batch["attention_mask"]
-                ).to(self.model.device)
+                    attention_mask=batch["attention_mask"],
+                    output_hidden_states=True  # 需要获取隐藏状态用于价值估计
+                )
                 
                 # 计算损失
-                loss = self.compute_loss(self.model, batch)
+                loss = self.compute_loss(self.model, self.critic_model, batch)
                 
                 # 反向传播
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(self.model.parameters(), train_args.max_grad_norm)
-                self.optimizer.step()
+                torch.nn.utils.clip_grad_norm_(self.critic_model.parameters(), train_args.max_grad_norm)
+                
+                self.optimizer_actor.step()
+                self.optimizer_critic.step()
                 
                 # 更新进度条
                 progress_bar.set_postfix(loss=loss.item())
             
-            # 每个epoch结束后保存模型
-            self.save_model() 
+            
+            # 将更新后的策略模型参数复制给参考模型
+            self.ref_model.load_state_dict(self.model.state_dict())
+            
+            
+        self.save_model() 
             
     def save_model(self):  
         # save_path = os.path.join(self.output_dir, "qwen2_ppo")  
